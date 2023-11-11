a20ac07b6fad9817277f119926ec1c579b6b9fec
Caio <caioarrudar.l2003@gmail.com>
2023-11-11 19:41:15 -0300

    Commit inicial ajustando estrutura do projeto

---
+++ comente_sobre/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for comente_sobre project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'comente_sobre.settings')
+
+application = get_wsgi_application()
---
+++ comente_sobres/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ComenteSobresConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'comente_sobres'
