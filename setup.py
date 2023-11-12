08efdd11c3b7fa381f957a9e0ce47d062f4b8841
Caio <caioarrudar.l2003@gmail.com>
2023-11-12 20:04:55 -0300

    abertura feature ajuste version

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='comente_sobre',
+    version='feature-telas-iniciais-SNAPSHOT',
+    description='Projeto para a disciplina de Engenharia de Software, visando a criação de um sistema de comentários.',
+)
