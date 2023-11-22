cfa345ba2cf08e141b5ffbc052c28728c389b364
Caio <caioarrudar.l2003@gmail.com>
2023-11-22 09:33:14 -0300

    db usuario, comentario e topico

--- comente_sobres/models.py
+++ comente_sobres/models.py
@@ -1,12 +1,27 @@
 from django.db import models
 
-class Topico(models.Model):
-    """Um assunto sobre o qual o usuário está interessado em comentar"""
-    id = models.AutoField(primary_key=True)
+from django.db import models
+
+class Usuario(models.Model):
     name = models.CharField(max_length=200)
-    date_added = models.DateTimeField(auto_now_add=True)
-    id_usuario_added = models.IntegerField()
+    email = models.EmailField(max_length=200)
 
     def __str__(self):
-        """Devolve uma representação em string do modelo."""
         return self.name
+
+class Topico(models.Model):
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    usuario_added = models.ForeignKey(Usuario, on_delete=models.CASCADE, default=None)
+
+    def __str__(self):
+        return self.name
+    
+class Comentario(models.Model):
+    valor = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    id_usuario_added = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    curtidas = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.valor
